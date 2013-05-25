fc64212a1c916052a6e58d3d33d7657e3a81411c
Joe Wu <joe7wu@athinkingape.com>
2013-05-24 23:35:47 -0700

    Created SettingsActivity and its layout; edited main menu.

--- dmm/src/main/java/joewu/dmm/MainActivity.java
+++ dmm/src/main/java/joewu/dmm/MainActivity.java
@@ -1,8 +1,10 @@
 package joewu.dmm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -16,8 +18,26 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public boolean onMenuItemSelected(int id, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                // Do something for create
+                return true;
+            case R.id.action_settings:
+                showSettingsActivity();
+                return true;
+            default:
+                return super.onMenuItemSelected(id, item);
+        }
+    }
+
+    private void showSettingsActivity() {
+        Intent intent = new Intent(getBaseContext(), SettingsActivity.class);
+        startActivity(intent);
     }
     
 }
---
+++ dmm/src/main/java/joewu/dmm/SettingsActivity.java
@@ -0,0 +1,16 @@
+package joewu.dmm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by joe7wu on 2013-05-24.
+ */
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
