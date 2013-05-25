30245194272b141c37d99130f25c1f4964625010
Joe Wu <joe7wu@athinkingape.com>
2013-05-25 00:03:50 -0700

    Added navigation up to the SettingsActivity.

--- dmm/src/main/java/joewu/dmm/SettingsActivity.java
+++ dmm/src/main/java/joewu/dmm/SettingsActivity.java
@@ -1,7 +1,10 @@
 package joewu.dmm;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 /**
  * Created by joe7wu on 2013-05-24.
@@ -12,5 +15,18 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
