975ede38911df0715a609efb90f2e0fd1aba5d70
Joe Wu <wqnvancer@gmail.com>
2013-05-28 07:57:54 -0700

    Added PreferenceScreen details.

--- dmm/src/main/java/joewu/dmm/SettingsActivity.java
+++ dmm/src/main/java/joewu/dmm/SettingsActivity.java
@@ -1,22 +1,25 @@
 package joewu.dmm;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.preference.PreferenceActivity;
 import android.view.MenuItem;
 
 /**
  * Created by joe7wu on 2013-05-24.
  */
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+//        setContentView(R.layout.activity_settings);
+
+        // set navigation-up using app icon
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        addPreferencesFromResource(R.layout.activity_settings);
     }
 
     @Override
@@ -29,4 +32,5 @@ public class SettingsActivity extends Activity {
                 return false;
         }
     }
+
 }
